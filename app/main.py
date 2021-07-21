d5b0156e76b504547b486fe97f8df81de16cdc13
tansubaktiran <87576344+tansubaktiran@users.noreply.github.com>
2021-07-21 21:55:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
   
 @app.route("/")
 def home_view():
-    script=server_document("http://localhost:5006/flaskbokeh1")
+    script=server_document("flaskbokeh1")
     return render_template('flaskbokeh1.html',bokS=script)
