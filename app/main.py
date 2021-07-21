24c6765d2bbcdbe6c5c1018387ddde542733a9d1
tansubaktiran <87576344+tansubaktiran@users.noreply.github.com>
2021-07-21 22:02:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask
-  
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from bokeh.embed import server_document
+# import bokeh
 app = Flask(__name__)
-  
+
 @app.route("/")
-def home_view():
-    return render_template('flaskbokeh1.html')
+def main():
+    script=server_document("http://tflasktest1.herokuapp.com/app")
+    return render_template('flaskbokeh1.html',bokS=script)
+
+if __name__ == "__main__":
+    app.run()
+
+
+#bokeh serve serv_1.py --allow-websocket-origin=127.0.0.1:5000
